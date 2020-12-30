c8270991ae459f926eb0225e96677857a8e7e34b
Andrei Sorokin1 <andrei_sorokin1@epam.com>
2020-12-31 02:00:45 +0300

    Migrate to Paging library 3

--- app/src/main/java/bav/astrobirthday/ui/favorites/FavoritesViewModel.kt
+++ app/src/main/java/bav/astrobirthday/ui/favorites/FavoritesViewModel.kt
@@ -1,22 +1,29 @@
 package bav.astrobirthday.ui.favorites
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.paging.PagedList
-import androidx.paging.toLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.paging.*
 import bav.astrobirthday.common.UserPreferences
 import bav.astrobirthday.data.entities.PlanetDescription
 import bav.astrobirthday.data.local.PlanetDao
 import bav.astrobirthday.utils.toPlanetDescription
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class FavoritesViewModel(
     private val preferences: UserPreferences,
     private val database: PlanetDao
 ) : ViewModel() {
 
-    val planetsList: LiveData<PagedList<PlanetDescription>> = database.getFavoritePlanets().map {
-        it.toPlanetDescription(preferences.userBirthday!!)
-    }.toLiveData(
-        pageSize = 20
-    )
+    val planetsList: Flow<PagingData<PlanetDescription>> = Pager(
+        PagingConfig(pageSize = 20)
+    ) {
+        database.getFavoritePlanets()
+    }.flow
+        .map { data ->
+            data.map {
+                it.toPlanetDescription(preferences.userBirthday!!)
+            }
+        }
+        .cachedIn(viewModelScope)
 }
