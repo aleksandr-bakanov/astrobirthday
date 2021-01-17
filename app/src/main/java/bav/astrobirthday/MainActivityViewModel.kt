3031fe9d5c88ca804fc78d42c9c7e4af10a4e6cf
Andrey <asorokius@gmail.com>
2021-01-17 19:20:17 +0300

    Fix HomeFragment.kt showing 0 age on first launch (#14)
    Co-authored-by: Andrei Sorokin1 <andrei_sorokin1@epam.com>

--- app/src/main/java/bav/astrobirthday/MainActivityViewModel.kt
+++ app/src/main/java/bav/astrobirthday/MainActivityViewModel.kt
@@ -5,16 +5,12 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import bav.astrobirthday.MainActivityViewModel.MainViewEvent.AnimateBars
-import bav.astrobirthday.common.UserPreferences
+import bav.astrobirthday.common.InitData
 import bav.astrobirthday.ui.common.ViewEvent
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.filterNotNull
-import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.flow.take
 import kotlinx.coroutines.launch
 
 class MainActivityViewModel(
-    preferences: UserPreferences,
+    initData: InitData
 ) : ViewModel() {
 
     val state: LiveData<MainViewState>
@@ -28,18 +24,9 @@ class MainActivityViewModel(
     init {
         _state.value = MainViewState()
         viewModelScope.launch {
-            val isBirthdayAlreadySetup = preferences.birthdayFlow.firstOrNull() != null
-            if (isBirthdayAlreadySetup) {
-                _state.value = MainViewState(barsVisible = true)
-            } else {
-                preferences.birthdayFlow
-                    .filterNotNull()
-                    .take(1)
-                    .collect {
-                        _state.value = MainViewState(barsVisible = true)
-                        _events.value = AnimateBars()
-                    }
-            }
+            initData.waitForInit()
+            _state.value = MainViewState(barsVisible = true)
+            _events.value = AnimateBars()
         }
     }
 
