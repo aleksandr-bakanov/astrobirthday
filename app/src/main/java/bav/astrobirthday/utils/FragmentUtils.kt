ee310781453da2855e95cf4032a9baebba9c40f7
Andrei Sorokin1 <andrei_sorokin1@epam.com>
2021-01-19 23:48:19 +0300

    Add SearchView styles and themes

--- app/src/main/java/bav/astrobirthday/utils/FragmentUtils.kt
+++
@@ -1,9 +0,0 @@
-package bav.astrobirthday.utils
-
-import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
-import bav.astrobirthday.ui.common.NavUiConfigurator
-
-fun Fragment.setupToolbar(toolbar: Toolbar) {
-    (requireActivity() as NavUiConfigurator).setupToolbar(toolbar)
-}
